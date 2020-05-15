bee378dab24eed9c54023cc3644f0678c26c7fe9
José Ignacio Tapia <30639989+crashToxic@users.noreply.github.com>
2020-05-14 21:04:27 -0300

    Leyendo las Categrías de Eventos desde la REST API

--- js/eventbrite.js
+++ js/eventbrite.js
@@ -6,5 +6,14 @@ class EventBrite {
     // obtiene las categorías en init()
     async obtenerCategorias() {
         // consultar las categorias a la REST API de event brite
+        const respuestaCategorias = await fetch(`https://www.eventbriteapi.com/v3/categories/?token=${this.token_auth}`);
+
+        // esperar la respuesta de las categorias y devolver un JSON
+        const categorias = await respuestaCategorias.json();
+
+        // devolvemos el resultado
+        return {
+            categorias
+        }
     }
 }
--- js/interfaz.js
+++ js/interfaz.js
@@ -8,10 +8,24 @@ class Interfaz {
 
     // método para cuando inicialice la app
     init() {
-
+        // llamar a imprimir categorias de la REST API
+        this.imprimirCategorias();
     }
     // imprimir categorías
     imprimirCategorias() {
-        const listaCategorias = eventbrite.obtenerCategorias();
+        const listaCategorias = eventbrite.obtenerCategorias()
+            .then(categorias => {
+                const cats = categorias.categorias.categories;
+                // seleccionar el select de categorias
+                const selectCategoria = document.getElementById('listado-categorias');
+
+                // recorremos el arreglo e imprimimos los <option>
+                cats.forEach(cat => {
+                    const option = document.createElement('option');
+                    option.value = cat.id;
+                    option.appendChild(document.createTextNode(cat.name_localized));
+                    selectCategoria.appendChild(option);
+                });
+            })
     }
 }
